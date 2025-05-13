d976ddc24ee8bd8199373923df946bb01f4955d2
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-13 12:56:43 +0200

    insert services directly into server configuration

--- server/srv-prod-1/default.nix
+++ server/srv-prod-1/default.nix
@@ -1,10 +1,14 @@
-{ ...}: {
-  imports =
-    [
-      ./disk-config.nix
-      ./hardware-configuration.nix
+{ hostname, ... }:
 
-      # services
-    ];
-  #system.stateVersion = 24.11;
+let
+  services = [ "" ];
+in
+{
+  imports = [
+    ./disk-config.nix
+    ./hardware-configuration.nix
+  ] ++ builtins.map (serviceName: ../../services/${serviceName}.nix) services;
+  #] ++ builtins.map (serviceName: ../../services/${serviceName}.nix { inherit hostname; }) services;
+
+  # system.stateVersion = 24.11;
 }
