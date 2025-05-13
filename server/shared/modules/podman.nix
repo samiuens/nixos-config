b64d490ee916fe20556f2ecfcd22627eba8bcbca
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-13 20:26:36 +0200

    removed podman

--- server/shared/modules/podman.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  virtualisation.containers.enable = true;
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings.dns_enabled = true;
-  };
-}
