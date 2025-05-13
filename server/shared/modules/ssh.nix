21222574ac1b64034bfdaf702298f5988d785114
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-13 20:26:53 +0200

    open firewall for ssh

--- server/shared/modules/ssh.nix
+++ server/shared/modules/ssh.nix
@@ -1,6 +1,7 @@
 { hostname, username, ... }: {
   services.openssh = {
     enable = true;
+    openFirewall = true;
     settings = {
       PasswordAuthentication = false;
       AllowUsers = [ ${username} ];
