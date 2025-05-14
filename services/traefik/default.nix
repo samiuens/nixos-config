cfc1c9a27cd216078e3643be6a76f732b2cffe84
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-15 00:58:33 +0200

    changed traefik

--- services/traefik/default.nix
+++ services/traefik/default.nix
@@ -1,14 +1,25 @@
-{ config, hostname, baseDomain, ... }: 
+{ config, pkgs, hostname, baseDomain, ... }: 
   let 
     name = "traefik";
     image = "traefik:v3.4";
     domain = "traefik.${hostname}.${baseDomain}";
+    
+    # Container files
+    traefikConfig = pkgs.writeTextFile {
+      name = "traefik.yaml";
+      destination = "/traefik.yaml";
+      text = builtins.readFile(./traefik.yaml);
+    };
   in {
     # Allow HTTP (80), HTTPS (443), and Traefik Dashboard (8080)
     networking.firewall.allowedTCPPorts = [ 80 443 8080 ];
 
-    # Use Podman as the container backend
-    virtualisation.oci-containers.backend = "podman";
+    # Defining secrets
+    sops.secrets.cloudflare_api_key = {};
+    sops.secrets.traefik_dashboard_credentials = {};
+
+    # Use Docker as the container backend
+    virtualisation.oci-containers.backend = "docker";
 
     # Define the container
     virtualisation.oci-containers.containers."${name}" = {
@@ -16,32 +27,29 @@
       ports = [ "80:80" "443:443" "8080:8080" ];
       volumes = [
         "/var/run/docker.sock:/var/run/docker.sock:ro"
-        "/etc/traefik:/etc/traefik"
+        "${traefikConfig}:/traefik.yaml:ro"
       ];
       environment = {
-        CF_API_KEY = ${config.sops.secrets.CLOUDFLARE_API_KEY};
-        TRAEFIK_DASHBOARD_CREDENTIALS = "${config.sops.secrets.TRAEFIK_DASHBOARD_CREDENTIALS}";
+        CF_API_KEY = "${config.sops.secrets.cloudflare_api_key.path}";
+        TRAEFIK_DASHBOARD_CREDENTIALS = "${config.sops.secrets.traefik_dashboard_credentials.path}";
       };
-      binds = [
-        "${./config/traefik.yaml}:/etc/traefik/traefik.yaml"
-      ];
-      labels = [
-        "traefik.enable=true"
-        "traefik.http.routers.traefik.entrypoints=http"
-        "traefik.http.routers.traefik.rule=Host(`${domain}`)"
-        "traefik.http.middlewares.traefik-auth.basicauth.users=${config.sops.secrets.TRAEFIK_DASHBOARD_CREDENTIALS}"
-        "traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme=https"
-        "traefik.http.middlewares.sslheader.headers.customrequestheaders.X-Forwarded-Proto=https"
-        "traefik.http.routers.traefik.middlewares=traefik-https-redirect"
-        "traefik.http.routers.traefik-secure.entrypoints=https"
-        "traefik.http.routers.traefik-secure.rule=Host(`${domain}`)"
-        "traefik.http.routers.traefik-secure.middlewares=traefik-auth"
-        "traefik.http.routers.traefik-secure.tls=true"
-        "traefik.http.routers.traefik-secure.tls.certresolver=cloudflare"
-        "traefik.http.routers.traefik-secure.tls.domains[0].main=${hostname}.${baseDomain}"
-        "traefik.http.routers.traefik-secure.tls.domains[0].sans=*.${hostname}.${baseDomain}"
-        "traefik.http.routers.traefik-secure.service=api@internal"
-      ];
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.traefik.entrypoints" = "http";
+        "traefik.http.routers.traefik.rule" = "Host(`${domain}`)";
+        #"traefik.http.middlewares.traefik-auth.basicauth.users=${config.sops.secrets.traefik_dashboard_credentials}"
+        "traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme" = "https";
+        "traefik.http.middlewares.sslheader.headers.customrequestheaders.X-Forwarded-Proto" = "https";
+        "traefik.http.routers.traefik.middlewares" = "traefik-https-redirect";
+        "traefik.http.routers.traefik-secure.entrypoints" = "https";
+        "traefik.http.routers.traefik-secure.rule" = "Host(`${domain}`)";
+        "traefik.http.routers.traefik-secure.middlewares" = "traefik-auth";
+        "traefik.http.routers.traefik-secure.tls" = "true";
+        "traefik.http.routers.traefik-secure.tls.certresolver" = "cloudflare";
+        "traefik.http.routers.traefik-secure.tls.domains[0].main" = "${hostname}.${baseDomain}";
+        "traefik.http.routers.traefik-secure.tls.domains[0].sans" = "*.${hostname}.${baseDomain}";
+        "traefik.http.routers.traefik-secure.service" = "api@internal";
+      };
       extraOptions = [ "--network=${hostname}" ];
     };
-  };
+  }
