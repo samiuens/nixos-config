c97a7181004cb19d52eb56bd2f839f3869ae1ff1
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-15 00:02:48 +0200

    pinned to 24.11

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "samiarda's server configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
