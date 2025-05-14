9a6beeaf6ecc14dc96fba6b371d0debb232e0f5a
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-14 22:02:13 +0200

    improved nix settings

--- server/shared/modules/nix.nix
+++ server/shared/modules/nix.nix
@@ -1,8 +1,14 @@
-{ ... }: {
+{ username, platform, lib, ... }: {
   nix = {
     enable = true;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings.trusted-users = [ "${username}" ];
+  };
+  nixpkgs = {
+    config.allowUnfree = true;
+    config.allowBroken = true;
+    hostPlatform = lib.mkDefault "${platform}";
   };
 }
