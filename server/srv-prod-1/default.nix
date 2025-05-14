ecd226cb3d94f887072c5e697fcb88c08550754f
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-15 00:58:41 +0200

    added traefik to 'srv-prod-1'

--- server/srv-prod-1/default.nix
+++ server/srv-prod-1/default.nix
@@ -1,14 +1,14 @@
 { hostname, ... }:
 
 let
-  services = [];
+  services = [ "traefik"];
 in
 {
   imports = [
     ./disk-config.nix
     ./hardware-configuration.nix
-  ] ++ builtins.map (serviceName: ../../services/${serviceName}.nix) services;
-  #] ++ builtins.map (serviceName: ../../services/${serviceName}.nix { inherit hostname; }) services;
+  ] ++ builtins.map (serviceName: ../../services/${serviceName}) services;
+  #] ++ builtins.map (serviceName: ../../services/${serviceName} { inherit hostname; }) services;
 
   system.stateVersion = "24.11";
 }
