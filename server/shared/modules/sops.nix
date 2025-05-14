2572c0cd522fa2c38144ba6479b80cad06784473
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-15 00:40:10 +0200

    sops configuration

--- server/shared/modules/sops.nix
+++ server/shared/modules/sops.nix
@@ -1,6 +1,4 @@
-{ hostname, ... }:
-{
-  sops = {
-    defaultSopsFile = ../../../secrets/public/${hostname}.yaml;
-  };
+{ hostname , ...}: {
+  sops.defaultSopsFile = ../../../secrets/public/${hostname}.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 }
