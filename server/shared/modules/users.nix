2191b15dbbdf8d22674b1fd76e0ab45c0aa48487
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-14 22:26:08 +0200

    added password to user

--- server/shared/modules/users.nix
+++ server/shared/modules/users.nix
@@ -8,5 +8,6 @@
       ../../../secrets/public/${hostname}-keychain.pub
       ../../../secrets/public/${hostname}-backup.pub
     ];
+    hashedPassword = "$6$gsjBDZd8jmXtEZHv$Eta7mLPSvaoOTXUIAuT68OFOevXQUqOjE3M4x2zOHrk9AfPifhJ68u//sgUAtYjTE1Vhe5ZZon.647xss.Rrq.";
   };
 }
