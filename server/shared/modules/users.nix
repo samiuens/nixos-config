1206c91d47dbfee5110d11a8e8414c0ed716cfca
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-14 22:32:47 +0200

    removed hashed password due to mistake

--- server/shared/modules/users.nix
+++ server/shared/modules/users.nix
@@ -8,6 +8,5 @@
       ../../../secrets/public/${hostname}-keychain.pub
       ../../../secrets/public/${hostname}-backup.pub
     ];
-    hashedPassword = "$6$gsjBDZd8jmXtEZHv$Eta7mLPSvaoOTXUIAuT68OFOevXQUqOjE3M4x2zOHrk9AfPifhJ68u//sgUAtYjTE1Vhe5ZZon.647xss.Rrq.";
   };
 }
