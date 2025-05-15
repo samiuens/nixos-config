72ceb26436f27d0e88bde9f40b727a5e18d60457
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-15 16:49:08 +0200

    switched homepage to docker container

--- services/homepage/default.nix
+++ services/homepage/default.nix
@@ -1,11 +1,35 @@
-{ hostname, baseDomain, ... }: 
+{ config, pkgs, hostname, baseDomain, username, ... }: 
   let 
-    name = "homepage-dashboard";
-    domain = "home.${baseDomain}";
-  in {
-    services.${name} = {
-      enable = true;
-      domain = domain;
-      openFirewall = false;
+    name = "homepage";
+    image = "ghcr.io/gethomepage/homepage:latest";
+    domain = "home.${hostname}.${baseDomain}";
+    
+    # Container files
+    homepageSettings = pkgs.writeTextFile {
+      name = "settings.yaml";
+      text = builtins.readFile ./settings.yaml;
     };
-  };
+  in {
+    # Use Docker as the container backend
+    virtualisation.oci-containers.backend = "docker";
+
+    # Define the container
+    virtualisation.oci-containers.containers."${name}" = {
+      image = "${image}";
+      ports = [ "80:80" "443:443" ];
+      volumes = [
+        "${homepageSettings}:/app/config/settings.yaml"
+        "/var/run/docker.sock:/var/run/docker.sock:ro"
+      ];
+      environment = {
+        HOMEPAGE_ALLOWED_HOSTS = "${domain}";
+      };
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.${name}.rule" = "Host(`${domain}`)";
+        "traefik.http.routers.${name}.entrypoints" = "https";
+        "traefik.http.routers.${name}.tls" = "true";
+      };
+      extraOptions = [ "--network=${hostname}" ];
+    };
+  }
