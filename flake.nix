7c4de465431c76ad28fad1d970ef1b6a06b8cbae
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-19 14:14:32 +0200

    changed home manager version

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
