a2b70e79ecfbd9fc9020b5b42aa36bcad5ed0dbc
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-19 15:36:04 +0200

    disabled docker network creation

--- systems/server/modules/docker.nix
+++ systems/server/modules/docker.nix
@@ -2,11 +2,10 @@
   virtualisation.docker.enable = true;
 
   # Create docker network if it doesn't exist
-  systemd.services."docker-${hostname}_network" = {
+  /*systemd.services."docker-${hostname}_network" = {
     description = "Create docker network for ${hostname}.";
     after = [ "docker.service" ];
     wantedBy = [ "multi-user.target" ];
-    conditionPathExists = "!/var/run/docker/netns/${hostname}";
 
     serviceConfig = {
       Type = "oneshot";
@@ -14,5 +13,5 @@
       ExecStart = "${pkgs.docker}/bin/docker network create --driver bridge ${hostname}";
       ExecStop = "${pkgs.docker}/bin/docker network rm ${hostname}";
     };
-  };
+  };*/
 }
