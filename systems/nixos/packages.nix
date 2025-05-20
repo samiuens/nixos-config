07c566d3731211eddbe5bf91d20b26a72a89720a
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-20 17:55:01 +0200

    🚀 Add applications and packages to 'nixos'

--- systems/nixos/packages.nix
+++ systems/nixos/packages.nix
@@ -3,13 +3,32 @@
 with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
-  # productivity
+  # consume
   firefox
+
+  # create
+  obsidian
+  anki
+  drawio
+
+
+  # communication
+  discord
 
   # development
   gitkraken
   code-cursor
-  
+  jetbrains.idea-ultimate
+  jetbrains.datagrip
+
+  # hosting
+  termius
+
+  # security
+  yubioath-flutter
+  keepassxc
+  cryptomator
+
   # tweaks
   dconf-editor
 ]
