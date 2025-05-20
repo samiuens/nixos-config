eaddca389376f81fe3220ea2f7b4b23d95d71caa
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-20 20:37:41 +0200

    🚀 Add node modules and terminal tools as packages to 'shared'

--- systems/shared/packages.nix
+++ systems/shared/packages.nix
@@ -3,8 +3,17 @@ with pkgs; [
   # frameworks
   jdk24
   nodejs_24
+  typescript
+
+  # global node modules
+  nodemon
+  nodePackages_latest.ts-node1
+
+  # terminal tools
+  tmux
 
   # tools
+  yubikey-manager
   just
   statix
 ]
