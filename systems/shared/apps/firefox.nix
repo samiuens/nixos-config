473531d0c0eab41bf35684dd0566c7905a4cd28e
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-21 21:37:21 +0200

    🚀 Install firefox extensions

--- systems/shared/apps/firefox.nix
+++ systems/shared/apps/firefox.nix
@@ -22,6 +22,24 @@
       (lib.mkIf pkgs.stdenv.isLinux pkgs.firefox)
       (lib.mkIf pkgs.stdenv.isDarwin null)
     ];
+
+    policies = {
+      ExtensionSettings = with builtins;
+        let extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+        };
+        in listToAttrs [
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+          (extension "1password-x-password-manager" "{d634138d-c276-4fc8-924b-40a0ea21d284}")
+          (extension "raindropio" "jid0-adyhmvsP91nUO8pRv0Mn2VKeB84@jetpack")
+          (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
+          (extension "sponsorblock" "sponsorBlocker@ajay.app")
+        ];
+    };
     profiles.samiuensay =
       let profileSettings = {
         # firefox sync
