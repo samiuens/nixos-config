a6f45ba0528bf3c3933b1e52e6f146d02f9e7d73
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-21 12:05:23 +0200

    🚚  Move nixos packages to 'shared' due to being shared between nixos and darwin.

--- systems/nixos/packages.nix
+++ systems/nixos/packages.nix
@@ -3,37 +3,6 @@
 with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
-  # create
-  obsidian
-  anki
-  drawio
-
-
-  # communication
-  discord
-
-  # development
-  gitkraken
-  code-cursor
-  jetbrains.idea-ultimate
-  jetbrains.datagrip
-
-  # hosting
-  termius
-
-  # package managers
-  corepack_24
-  pnpm
-
-  # cli
-  nest-cli
-  awscli2
-
-  # security
-  yubioath-flutter
-  keepassxc
-  cryptomator
-
   # tweaks
   dconf-editor
 ]
--- systems/shared/packages.nix
+++ systems/shared/packages.nix
@@ -2,6 +2,36 @@
 
 # These packages are shared between nixos and darwin.
 with pkgs; [
+  # create
+  obsidian
+  anki
+  drawio
+
+  # communication
+  discord
+
+  # hosting
+  termius
+
+  # package managers
+  corepack_24
+  pnpm
+
+  # cli
+  nest-cli
+  awscli2
+
+  # security
+  yubioath-flutter
+  keepassxc
+  cryptomator
+
+  # development
+  gitkraken
+  code-cursor
+  jetbrains.idea-ultimate
+  jetbrains.datagrip
+
   # frameworks
   jdk24
   nodejs_24
