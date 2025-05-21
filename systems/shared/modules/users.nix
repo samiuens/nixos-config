1116501ef4adf15196e6680c0626b315fe5615ed
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-21 20:01:44 +0200

    🐛 Use 'zsh' as shell; quirk needed

--- systems/shared/modules/users.nix
+++ systems/shared/modules/users.nix
@@ -1,6 +1,8 @@
-{ vars, username, hostname, ... }: {
+{ vars, username, hostname, pkgs, ... }: {
   users.users.${username} = {
     description = "${vars.fullName}";
     isNormalUser = true;
+    shell = pkgs.zsh;
+    ignoreShellProgramCheck = true; # needed, because nix doesn't detect home manager shell configuration
   };
 }
