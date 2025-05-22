94d7f054f05252581a097338dad6537be06ccb5f
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-22 19:08:05 +0200

    ⏪ Remove gnome application launcher hot-fix

--- systems/nixos/home/default.nix
+++ systems/nixos/home/default.nix
@@ -7,8 +7,6 @@
       ../gnome/config.nix
       ../../shared/config/git.nix
       ../../shared/config/shell.nix
-      # Quirks
-      ./gnome-packages.nix
     ];
   home.packages = pkgs.callPackage ../packages.nix {};
 }
