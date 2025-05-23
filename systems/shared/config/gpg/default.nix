00aef8fbc18522c04d64b1b361314651b6449dbb
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-23 16:54:36 +0200

    🐛 Corrected gpg config

--- systems/shared/config/gpg/default.nix
+++ systems/shared/config/gpg/default.nix
@@ -1,9 +1,32 @@
-{ config, ...}:
-  let
-    gpgConfigFile = builtins.readFile ./gpg.conf; in
-  {
-    programs.gnupg = {
-      enable = true;
-      settings = gpgConfigFile;
+{ config, pkgs, lib, ... }:
+
+{
+  programs.gpg = {
+    enable = true;
+    #agent.enable = true;
+
+    settings = {
+      personal-cipher-preferences = [ "AES256" "AES192" "AES" ];
+      personal-digest-preferences = [ "SHA512" "SHA384" "SHA256" ];
+      personal-compress-preferences = [ "ZLIB" "BZIP2" "ZIP" "Uncompressed" ];
+      default-preference-list = [ "SHA512" "SHA384" "SHA256" "AES256" "AES192" "AES" "ZLIB" "BZIP2" "ZIP" "Uncompressed" ];
+      cert-digest-algo = "SHA512";
+      s2k-digest-algo = "SHA512";
+      s2k-cipher-algo = "AES256";
+      charset = "utf-8";
+      no-comments = true;
+      no-emit-version = true;
+      no-greeting = true;
+      keyid-format = "0xlong";
+      list-options = [ "show-uid-validity" ];
+      verify-options = [ "show-uid-validity" ];
+      with-fingerprint = true;
+      require-cross-certification = true;
+      require-secmem = true;
+      no-symkey-cache = true;
+      armor = true;
+      use-agent = true;
+      throw-keyids = true;
     };
-  }
+  };
+}
