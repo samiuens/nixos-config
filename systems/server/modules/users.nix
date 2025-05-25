8a61bf27479f3a9dcfa4d8387b2167ad72e46df5
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-25 16:28:33 +0200

    👥 Add 'group' for users, to servers

--- systems/server/modules/users.nix
+++ systems/server/modules/users.nix
@@ -1,10 +1,16 @@
 { username, hostname, ...}: {
   users.users.${username} = {
     home = "/home/${username}";
+    group = "${username}";
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     openssh.authorizedKeys.keyFiles = [
       ../../../secrets/public/${hostname}-keychain.pub
       ../../../secrets/public/${hostname}-backup.pub
     ];
+  };
+  users.groups = {
+    "${username}" = {
+      gid = 1000;
+    };
   };
 }
