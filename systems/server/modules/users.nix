7cfbc53941e863b3a57bb8f19574e0aa3d311142
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-25 21:57:34 +0200

    👥 Remove user group for 'samiarda'

--- systems/server/modules/users.nix
+++ systems/server/modules/users.nix
@@ -1,16 +1,10 @@
 { username, hostname, ...}: {
   users.users.${username} = {
     home = "/home/${username}";
-    group = "${username}";
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    extraGroups = [ "root" "wheel" "networkmanager" "docker" ];
     openssh.authorizedKeys.keyFiles = [
       ../../../secrets/public/${hostname}-keychain.pub
       ../../../secrets/public/${hostname}-backup.pub
     ];
-  };
-  users.groups = {
-    "${username}" = {
-      gid = 1000;
-    };
   };
 }
