2cfc2674bb48945555983e101a47e39d8faa8b11
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-25 02:41:30 +0200

    🚑 Fix ssh-key authentication with yubikey

--- systems/shared/config/ssh.nix
+++ systems/shared/config/ssh.nix
@@ -5,9 +5,12 @@
       "srv-prod-1" = {
         hostname = "srv-prod-1.samiarda.com";
         user = "samiarda";
+        identitiesOnly = true;
         identityFile = "~/.ssh/srv-prod-1-keychain";
         #identityFile = "~/.ssh/srv-prod-1-backup";
+        identityAgent = "none";
       };
     };
   };
+  #services.ssh-agent.enable = true;
 }
