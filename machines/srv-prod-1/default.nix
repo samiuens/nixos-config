be3f01c5e8587a0d8030a1e419097c3ae50ea1b0
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-25 20:30:10 +0200

    🚀 Implemented 'sabnzbd'

--- machines/srv-prod-1/default.nix
+++ machines/srv-prod-1/default.nix
@@ -1,13 +1,20 @@
 { hostname, ... }:
 
 let
-  services = [ "netdata" "traefik" ];
+  services = [ "netdata" "traefik" "sabnzbd" ];
 in
 {
   imports = [
     ./disk-config.nix
     ./hardware-configuration.nix
   ] ++ builtins.map (serviceName: ../../systems/server/services/${serviceName}) services;
+
+  # Mount external hard drive for media storage;
+  fileSystems."/mnt/media" = {
+    device = "/dev/disk/by-uuid/78f433f8-4560-428f-bd3f-df9872a53706";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
 
   system.stateVersion = "24.11";
 }
--- systems/server/services/sabnzbd/default.nix
+++ systems/server/services/sabnzbd/default.nix
@@ -1,8 +1,36 @@
-{ hostname, ... }: 
-  let name = "sabnzbd";
-    domain = "https://${name}.${hostname}.samiarda.com";
+{ vars, hostname, username, ... }:
+  let 
+    name = "sabnzbd";
+    image = "lscr.io/linuxserver/sabnzbd:latest";
+    domain = "sabnzbd.${hostname}.${vars.domain}";
+    volumePath = "/srv/${name}";
   in {
-    services.${name} = {
-      enable = true;
+  #imports = [ ../../utils/copy-file.nix ];
+  # Configuration files
+  /*services.copyFile.sabnzbdConfig = {
+    source = builtins.toFile "sabnzbd.ini" (builtins.readFile ./config/sabnzbd.ini);
+    destination = "${volumePath}/config/sabnzbd.ini";
+    before = [ "docker-${name}.service" ];
+  };*/
+
+  # Use Docker as the container backend
+  virtualisation.oci-containers.backend = "docker";
+  # Define the container
+  virtualisation.oci-containers.containers."${name}" = {
+    image = "${image}";
+    volumes = [
+      "${volumePath}/config:/config"
+      "${volumePath}/downloads/incomplete:/incomplete-downloads"
+      "${volumePath}/downloads/complete:/downloads"
+      "/mnt/media:/mnt/media"
+    ];
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.${name}.rule" = "Host(`${domain}`)";
+      "traefik.http.routers.${name}.entrypoints" = "https";
+      "traefik.http.routers.${name}.tls" = "true";
+      "traefik.http.services.${name}.loadbalancer.server.port" = "8080";
     };
+    extraOptions = [ "--network=${hostname}" ];
   };
+}
