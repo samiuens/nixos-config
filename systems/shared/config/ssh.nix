0ca69fde6b59a3907a0ec3b9fb6827a4895c7b93
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-29 19:44:29 +0200

    🔐 Add github ssh key authentication to ssh config

--- systems/shared/config/ssh.nix
+++ systems/shared/config/ssh.nix
@@ -2,12 +2,20 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/github-auth_keychain";
+        #identityFile = "~/.ssh/github-auth_backup";
+        identityAgent = "none";
+      };
       "srv-prod-1" = {
         hostname = "srv-prod-1.samiarda.com";
         user = "samiarda";
         identitiesOnly = true;
-        identityFile = "~/.ssh/srv-prod-1-keychain";
-        #identityFile = "~/.ssh/srv-prod-1-backup";
+        identityFile = "~/.ssh/srv-prod-1_keychain";
+        #identityFile = "~/.ssh/srv-prod-1_backup";
         identityAgent = "none";
       };
     };
