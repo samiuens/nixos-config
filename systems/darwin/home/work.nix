3793b78ebe5fa6ae3fb0ef84dfb10257680572cf
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-29 14:08:34 +0200

    ✨ Ported 'work hm settings to darwin

---
+++ systems/darwin/home/work.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  imports = 
+    [
+      # Applications
+      ../../shared/apps/kitty.nix
+      # Configs
+      ../gnome/config.nix
+      ../../shared/config/shell
+    ];
+  home.packages = with pkgs; [
+    google-chrome
+    bitwarden-desktop
+    code-cursor
+    jetbrains.datagrip
+    gitkraken
+    nest-cli
+    awscli2
+    nodejs_24
+    typescript
+  ];
+}
