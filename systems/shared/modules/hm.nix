158c04d30c1dcb3706692565ea8beee4e320378a
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-05-29 13:57:38 +0200

    🐛 Fix import bug

--- systems/shared/modules/hm.nix
+++ systems/shared/modules/hm.nix
@@ -2,7 +2,7 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.${username} = import ../home { inherit username system pkgs lib; };
-  home-manager.users.${vars.work.username} = lib.mkIf work (import ../home { inherit username system pkgs lib; });
+  home-manager.users.${vars.work.username} = lib.mkIf work (import ../home/work.nix { inherit username system pkgs lib vars; });
   home-manager.backupFileExtension = "hm-backup";
   home-manager.extraSpecialArgs = {
     inherit hostname username platform system vars;
