18537de61bbcdd210800c79b0c66fe2dae536d5e
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-04 21:29:39 +0200

    🔧 Basic noice configuration

--- systems/shared/config/nixvim/plugins/ui/noice.nix
+++ systems/shared/config/nixvim/plugins/ui/noice.nix
@@ -1,5 +1,17 @@
 {
   programs.nixvim.plugins.noice = {
     enable = true;
+    settings = {
+      lsp.overrides = {
+        "vim.lsp.util.convert_input_to_markdown_line" = true;
+        "vim.lsp.util.stylize_markdown" = true;
+        #"cmp.entry.get_documentation" = true;
+      };
+      presets = {
+        "bottom_search" = true;
+        "command_palette" = true;
+        "long_message_to_split" = true;
+      };
+    };
   };
 }
