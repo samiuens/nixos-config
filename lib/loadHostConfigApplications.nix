69ddd3ea73adefba5046c436e7188cf330dff5fa
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-08 00:25:07 +0200

    🥅 Send warning if application configuration doesn't exist

--- lib/loadHostConfigApplications.nix
+++ lib/loadHostConfigApplications.nix
@@ -1,5 +1,7 @@
-{lib, ... }: {
-  loadHostConfigApplications = hostConfig: folderPath:
+{ lib, ... }:
+
+{
+  loadHostConfigApplications = applicationConfig: folderPath:
     let
       appModulePaths = lib.filter
         (path:
@@ -10,16 +12,22 @@
         )
         (builtins.map (name: "${folderPath}/${name}") (builtins.attrNames (builtins.readDir folderPath)));
 
-      activeAppImports = lib.filter (appModule: appModule != { }) (
-        lib.map (appPath:
-          let
-            appName = lib.removeSuffix ".nix" (builtins.baseNameOf appPath);
-          in
-          if lib.getAttrFromPath [ appName ] hostConfig.applications or false
-          then appPath
-          else { }
-        ) appModulePaths
-      );
+      foundAppNames = lib.map (path: lib.removeSuffix ".nix" (builtins.baseNameOf path)) appModulePaths;
+
+      checkedAppImports = lib.mapAttrs (appName: isEnabled:
+        if isEnabled then
+          if lib.elem appName foundAppNames
+          then (
+            lib.findSingle
+              (path: lib.removeSuffix ".nix" (builtins.baseNameOf path) == appName)
+              appModulePaths
+          )
+          else lib.warn "WARN: The application '${appName}' is activated in your config, but the file '${folderPath}/${appName}.nix' doesnt exist!" { }
+        else
+          { }
+      ) applicationConfig;
+
+      activeAppImports = lib.filter (appModule: appModule != { }) (lib.attrValues checkedAppImports);
     in
     activeAppImports;
 }
