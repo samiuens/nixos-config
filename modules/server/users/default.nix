74e1419d51abb6c32794d0c04b10f9a3cec3be52
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-08 01:40:20 +0200

    🐛 Fix import for user ssh key files

--- modules/server/users/default.nix
+++ modules/server/users/default.nix
@@ -6,8 +6,8 @@
     home = "/home/${hostConfig.user.username}";
     extraGroups = [ "root" "wheel" "networkmanager" "docker" ];
     openssh.authorizedKeys.keyFiles = [
-      ../../../secrets/public/${hostname}-keychain.pub
-      ../../../secrets/public/${hostname}-backup.pub
+      ../../../secrets/public/${hostConfig.hostname}-keychain.pub
+      ../../../secrets/public/${hostConfig.hostname}-backup.pub
     ];
     shell = pkgs.zsh;
     ignoreShellProgramCheck = true; # quirk due to nix not detecting shell configuration via hm
