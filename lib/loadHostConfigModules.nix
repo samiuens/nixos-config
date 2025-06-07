ff005ea0095912d50f66bb91b1f2568884a7c037
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-08 00:45:16 +0200

    🚚 Rename util function to fit more general purpose

---
+++ lib/loadHostConfigModules.nix
@@ -0,0 +1,35 @@
+{ lib, ... }: {
+  loadHostConfigModules = hostConfig: folderPath:
+    let
+      modulePaths = lib.filter
+        (path:
+          let
+            baseName = builtins.baseNameOf path;
+          in
+          lib.hasSuffix ".nix" baseName
+        )
+        (builtins.map (name: "${folderPath}/${name}") (builtins.attrNames (builtins.readDir folderPath)));
+
+      foundModuleNames = lib.map (path: lib.removeSuffix ".nix" (builtins.baseNameOf path)) modulePaths;
+
+      checkedModuleImports = lib.mapAttrs (moduleName: isEnabled:
+        if isEnabled then
+          if lib.elem moduleName foundModuleNames
+          then (
+            let
+              matchingPaths = lib.filter
+                (path: lib.removeSuffix ".nix" (builtins.baseNameOf path) == moduleName)
+                modulePaths;
+              modulePath = builtins.head matchingPaths;
+            in
+            import modulePath
+          )
+          else lib.warn "WARN: The module '${moduleName}' is activated in the config, but the file '${folderPath}/${moduleName}.nix' doesnt exist!" { }
+        else
+          { }
+      ) hostConfig;
+
+      activeModuleImports = lib.filter (moduleModule: moduleModule != { }) (lib.attrValues checkedModuleImports);
+    in
+    activeModuleImports;
+}
