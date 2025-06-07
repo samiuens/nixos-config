3cac5e5f442e1d4d18455e002622cba1af0336a8
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-07 13:22:25 +0200

    🔨 Improve wording and hints on install script

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 if [ "$(uname)" == "Darwin" ]; then
   echo "> macos detected..."
-  echo "this script will prepare system for nix-darwin installation."
+  echo "this script will prepare the system for nix-darwin installation."
   read -n 1 -s -r -p "press any key to continue or ctrl+c to abort..."
-
+  echo ""
+  
   # command line tools
   echo "installing 'command line tools'."
   if [[ -e /Library/Developer/CommandLineTools/usr/bin/git ]]; then
     echo "'command line tools' already installed."
   else
-    # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
     touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
     PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
     softwareupdate -i "$PROD" --verbose
@@ -27,7 +27,8 @@ if [ "$(uname)" == "Darwin" ]; then
   curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm
   
   # complete
-  echo "run the following command to activate the nix configuration:"
+  echo "shell restart is needed."
+  echo "after that, run the following command to activate the nix configuration:"
   echo "sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .#"
   echo "enter the hostname corresponding to the config you want to apply."
 
