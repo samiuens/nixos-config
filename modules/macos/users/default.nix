7d4de4401e9857ea4049d95369123588d1dec7a8
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-08 13:52:35 +0200

    ✨ Re-implement work user on 'macos'

--- modules/macos/users/default.nix
+++ modules/macos/users/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, hostConfig, ... }: {
+{ pkgs, lib, hostConfig, ... }: {
   imports = [ ./hm.nix ];
   
   users.users.${hostConfig.user.username} = {
@@ -8,5 +8,14 @@
     ignoreShellProgramCheck = true; # quirk due to nix not detecting shell configuration via hm
     isHidden = false;
   };
+
+  users.users.${hostConfig.workUser.username} = lib.mkIf hostConfig.workUser.enabled {
+    description = "${hostConfig.workUser.name}";
+    home = "/Users/${hostConfig.workUser.username}";
+    shell = pkgs.zsh;
+    ignoreShellProgramCheck = true; # quirk due to nix not detecting shell configuration via hm
+    isHidden = false;
+  };
+
   system.primaryUser = "${hostConfig.user.username}";
 }
--- modules/macos/users/hm.nix
+++ modules/macos/users/hm.nix
@@ -9,6 +9,7 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     users.${hostConfig.user.username} = import ../../home { inherit pkgs lib myConfig hostConfig; };
+    users.${hostConfig.workUser.username} = lib.mkIf hostConfig.workUser.enabled (import ../../home/work.nix { inherit pkgs lib myConfig hostConfig; });
     backupFileExtension = "hm-backup";
     extraSpecialArgs = {
       inherit myConfig hostConfig;
