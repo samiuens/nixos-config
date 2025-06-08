f78129e9764a825320f3fddf5ecb77e4b57c61fa
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-08 14:09:25 +0200

    🐛 Fix 'macos-fix' for firefox

--- modules/home/applications/firefox/default.nix
+++ modules/home/applications/firefox/default.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, hostConfig, ... }: {
-  #imports = [] ++ (lib.optional pkgs.stdenv.isDarwin ./macos-fix.nix);
+  imports = [] ++ (lib.optional hostConfig.os == "macos" ./macos-fix.nix);
 
   programs.firefox = {
     enable = true;
