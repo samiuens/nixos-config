14f9b5c7f2e3895fa16680766114514592bc4650
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-08 13:48:21 +0200

    ✏️ Fix typo

--- modules/nixos/users/hm.nix
+++ modules/nixos/users/hm.nix
@@ -7,10 +7,7 @@
 }: {
   home-manager = {
     useGlobalPkgs = true;
-    useUserPackages = true;k.nix { inherit pkgs lib myConfig hostConfig; });
-    backupFileExtension = "hm-backup";
-    extraSpecialArgs = {
-
+    useUserPackages = true;
     users.${hostConfig.user.username} = import ../../home { inherit pkgs lib myConfig hostConfig; };
     users.${hostConfig.workUser.username} = lib.mkIf hostConfig.workUser.enabled (import ../../home/work.nix { inherit pkgs lib myConfig hostConfig; });
     backupFileExtension = "hm-backup";
