2ce247d0e758176ba4f91649ae1763b59e492e11
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-09 13:18:41 +0200

    🔧 Add 'git' config

--- modules/home/configs/git.nix
+++ modules/home/configs/git.nix
@@ -1,5 +1,7 @@
-{
+{ hostConfig, ... }: {
   programs.git = {
     enable = true;
+    userName  = "${hostConfig.user.name}";
+    userEmail = "192653549+samiuens@users.noreply.github.com";
   };
 }
